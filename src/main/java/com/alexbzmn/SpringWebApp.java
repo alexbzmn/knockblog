1d2f47adfbad16ba8ab2b4c386cbfc1c21a838d7
alexbzmn <crashius@gmail.com>
2017-07-15 20:56:10 +0200

    application skeleton added

---
+++ src/main/java/com/alexbzmn/SpringWebApp.java
@@ -0,0 +1,15 @@
+package com.alexbzmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by User on 7/15/2017.
+ */
+@SpringBootApplication
+public class SpringWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebApp.class, args);
+    }
+}
